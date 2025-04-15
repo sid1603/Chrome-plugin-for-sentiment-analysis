e6c81bb11c0bc616162205e56857b5094ca712da
sid1603 <sidd.priyadarshi687@gmail.com>
2025-04-15 10:45:51 +0530

    readme updated

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'youtube',
     version= '0.0.0',
-    author= 'Bappy',
-    author_email= 'entbappy73@gmail.com',
+    author= 'Sid',
+    author_email= 'sidd.priyadarshi687@gmail.com',
     packages= find_packages(),
     install_requires = []
 
